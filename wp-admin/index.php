3e9a1f7453f1f3063d922df951da3047a534c80a
mike-the-tower <candura01@gmail.com>
2021-12-27 23:16:25 +0100

    test ship 27.18

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -8,4 +8,10 @@
 
 /** Load WordPress Bootstrap */
 require_once __DIR__ . '/admin.php';
-header(__DIR__.'/edit.php');
+
+/** Load WordPress dashboard API */
+/**require_once ABSPATH . 'wp-admin/includes/dashboard.php';*/
+
+//wp_dashboard_setup();
+
+wp_enqueue_script( 'dashboard' );
